78de8747045f180fd22277073ab0737fb3f767d0
Фортуна Максим Александрович <FortunaMA@DOM201.LOCAL>
2024-12-07 12:06:39 +0700

    sec_comm

--- src/components/UserPages/UsersTable.vue
+++ src/components/UserPages/UsersTable.vue
@@ -1,4 +1,9 @@
 <template>
+    <div class="users-table__add">
+    <input v-model="newUserName" placeholder="Имя" />
+    <input v-model="newUserEmail" placeholder="Почта" />
+    <button @click="addUser()">Добавить</button>
+    </div>
     <div class="users-table">
       <div class="users-table__row">
         <div class="users-table__cell">id</div>
@@ -11,6 +16,8 @@
         <div class="users-table__cell">{{ user.name }}</div>
         <div class="users-table__cell">{{ user.email }}</div>
         <div class="users-table__cell">{{ user.created }}</div>
+        <div class="users-table__del"><button @click="deleteUser(user.id)">Удалить</button></div>
+        <div class="users-table__edt"><button @click="editUser(user.id)">Изменить</button></div>
       </div>
     </div>
     </template>
@@ -57,6 +64,36 @@
         created: createdMockDate.value,
       }
     ])
+
+    const newUserName = ref('')
+    const newUserEmail = ref('')
+
+    const addUser = () => {
+      if (newUserName.value && newUserEmail.value) {
+        const newUser: UserType = {
+          id: users.value.length,
+          name: newUserName.value,
+          email: newUserEmail.value,
+          created: createdMockDate.value,
+        }
+        
+        users.value.push(newUser)
+        
+        // Очистка полей ввода после добавления
+        newUserName.value = ''
+        newUserEmail.value = ''
+      } else {
+        alert("Пожалуйста, заполните все поля.");
+      }
+    }
+
+    const deleteUser = (userId: number) => {
+    users.value = users.value.filter(user => user.id !== userId);
+    }
+
+    const editUser = (userId: number) => {
+    users.value = users.value.filter(user => user.id !== userId);
+    }
     </script>
     
     <style scoped>
@@ -67,12 +104,34 @@
     
     .users-table__row {
       display: grid;
-      grid-template-columns: repeat(4, 1fr);
+      grid-template-columns: repeat(6, 1fr);
     }
     
     .users-table__cell {
       min-width: 120px;
       background-color: #888888;
+      border: 1px solid cornsilk;
+      text-align: center;
+      padding: 4px;
+    }
+
+    .users-table__del {
+      min-width: 120px;
+      background-color: #FF0000;
+      border: 1px solid cornsilk;
+      text-align: center;
+      padding: 4px;
+    }
+    .users-table__add {
+      min-width: 120px;
+      background-color: #00ff00;
+      border: 1px solid cornsilk;
+      text-align: center;
+      padding: 4px;
+    }
+    .users-table__edt {
+      min-width: 120px;
+      background-color: #0000FF;
       border: 1px solid cornsilk;
       text-align: center;
       padding: 4px;
